5e936f29eeee384f73c75eef08e4a1f7db89e2d3
00bullet <emanuel_raul_1996@hotmail.com>
2024-03-20 13:01:46 -0600

    add (ui): se agraga maqueta de login

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
-import  { Formulario }  from './component/formulario'
-import  { Home }  from './component/home'
-import { useState } from 'react'
-import { Button, } from '@material-ui/core';
-import './App.css';
+import {Login} from './component/login'
+function App(){
 
+  return(
+    
+ <Login/>
 
-
-function App() {
-
-  const [ user, setUser] = useState([])
-
-  return (
-  
-    <div className="App">
-      {
-      !user.length > 0
-      
-   
-   ? <Formulario setUser={setUser} />   
-      
-   : <Home user={user} setUser={setUser}/>
-
-   
-   }
-  <Button variant='contained'>Iniciar sesión</Button>
-   </div>
-  
   );
-  
 }
-
-export default App;
+export default App
